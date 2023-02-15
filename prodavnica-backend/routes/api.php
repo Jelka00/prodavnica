32e578b9e21933e08611c2b69a19e16d1b44d672
Nikola Jelic <nj20190121@student.fon.bg.ac.rs>
2023-02-15 09:32:12 -0500

    Kreirane migracije za tabele u bazi kao i dodavanje proizvoda preko API ruta

--- prodavnica-backend/routes/api.php
+++ prodavnica-backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
+Route::post('addProduct', [ProductController::class, 'addProduct']);
